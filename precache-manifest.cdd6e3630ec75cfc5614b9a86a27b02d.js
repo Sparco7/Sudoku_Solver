bf208e402487ed454369e469c99aa4468d296970
Sparco7 <barakbbg@gmail.com>
2020-06-03 16:57:00 +0300

    Updates

---
+++ precache-manifest.cdd6e3630ec75cfc5614b9a86a27b02d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd3e87a3b3131f28733e99bb139b480",
+    "url": "/Sudoku_Solver/index.html"
+  },
+  {
+    "revision": "5bc40ac7042838a0dc96",
+    "url": "/Sudoku_Solver/static/css/main.a11bb92b.chunk.css"
+  },
+  {
+    "revision": "94a2d802be3841594dd6",
+    "url": "/Sudoku_Solver/static/js/2.8f440b61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sudoku_Solver/static/js/2.8f440b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc40ac7042838a0dc96",
+    "url": "/Sudoku_Solver/static/js/main.2d3f328b.chunk.js"
+  },
+  {
+    "revision": "9aa483e8dc53fcece4f1",
+    "url": "/Sudoku_Solver/static/js/runtime-main.2c72de9e.js"
+  }
+]);
